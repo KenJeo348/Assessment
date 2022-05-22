fbe9e2a6d83620e16c5acda02544850026518ccb
KenJeo348 <jeongk@middleton.school.nz>
2022-05-23 10:57:40 +1200

    commit

--- Final_Maori_Quiz.py
+++ Final_Maori_Quiz.py
@@ -19,7 +19,8 @@ def yes_no(question_text):
 
         # Otherwise - show error
         else:
-            print("Please enter either Y/N or Yes/No")
+            print()
+            print("Please enter either Y/N or Yes/No\n")
 
 
 def instructions():
@@ -40,37 +41,46 @@ def instructions():
 
 
 def quiz_chooser():
-    choose_quiz = 0
-    while choose_quiz == 0:
-        # Ask the user if they want to continue to the quiz.
-        print("0: Quit the Program\n"
-              "1: Redirects to Days of the Week quiz\n"
-              "2: Redirects to Numbers 1-10 quiz")
-        choose_quiz = int(input("Choose one of the options above and press enter: "))
-        print()
-
-        # If the user inputs <enter>, output 'Program continues to quiz.
-        if choose_quiz == 0:
-            print(formatter("~", "Goodbye"))
-            exit()
-
-        # If the user inputs 'x', output 'You have quit the program', and a farewell message.
-        elif choose_quiz == 1:
-            score = 0
-            total_score = days_of_week_quiz(score)
-            print(f"Your total score was {total_score}/7")
+    valid = False
+    while not valid:
+        try:
+            # Ask the user if they want to continue to the quiz.
+            print("0: Quit the Program\n"
+                  "1: Redirects to Days of the Week quiz\n"
+                  "2: Redirects to Numbers 1-10 quiz\n")
+            choose_quiz = int(input("Choose one of the options above and press enter: "))
             print()
 
-        elif choose_quiz == 2:
-            score = 0
-            total_score = numbers_quiz(score)
-            print(f"Your total score was {total_score}/10")
-            print()
+            # If the user inputs <enter>, output 'Program continues to quiz.
+            if choose_quiz == 0:
+                print(formatter("~", "Goodbye"))
+                exit()
 
-        # If the user enters an invalid answer, output 'Please enter a valid answer'
-        else:
-            print("Please enter a valid answer. <enter> to play, or 'x' to quit.")
-            choose_quiz = 0
+            # If the user inputs 'x', output 'You have quit the program', and a farewell message.
+            elif choose_quiz == 1:
+                score = 0
+                total_score = days_of_week_quiz(score)
+                print(f"Your total score was {total_score}/7")
+                print()
+                valid = True
+
+            elif choose_quiz == 2:
+                score = 0
+                total_score = numbers_quiz(score)
+                print(f"Your total score was {total_score}/10")
+                print()
+                valid = True
+
+            # If the user enters an invalid answer, output 'Please enter a valid answer'
+            else:
+                print()
+                print(formatter("#", "Please enter a valid answer."))
+                print()
+
+        except ValueError:
+            print()
+            print(formatter("#", "Please enter a valid answer."))
+            print()
 
 
 def days_of_week_quiz(_quiz_score):
@@ -78,9 +88,12 @@ def days_of_week_quiz(_quiz_score):
     player_score = 0
     question_number = 0
     days = [["Monday", "Rahina"], ["Tuesday", "Ratu"], ["Wednesday", "Raapa"], ["Thursday", "Rapare"],
-            ["Friday", "Ramere"], ["Saturday", "Rahoroi"] , ["Sunday", "Ratapu"]]
+            ["Friday", "Ramere"], ["Saturday", "Rahoroi"], ["Sunday", "Ratapu"]]
     # Shuffling the (days) list in a random order for the quiz.
     random.shuffle(days)
+
+    print(formatter("-", "Welcome to the Days of the week quiz"))
+    print()
 
     # While loop repeating until the user has gone through all the days.
     while question_number < 7:
@@ -111,6 +124,9 @@ def numbers_quiz(_quiz_score):
     numbers = [[1, "tahi"], [2, "rua"], [3, "toru"], [4, "wha"], [5, "rima"], [6, "ono"],
                [7, "whitu"], [8, "waru"], [9, "iwa"], [10, "tekau"]]
     random.shuffle(numbers)
+
+    print(formatter("-", "Welcome to the Numbers quiz"))
+    print()
 
     while question_number < 10:
 
@@ -152,7 +168,7 @@ while play_again != "x":
     if play_again == "":
         quiz_chooser()
     else:
-        print("Please enter a valid answer")
+        print("Please enter a valid answer\n")
     play_again = input("Do you want to play again (<enter> to play, or 'x' to quit): ")
     print()
 print(formatter("~", "Goodbye"))
